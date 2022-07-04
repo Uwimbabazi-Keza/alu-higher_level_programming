cfcdc499e00338c2e914145e6e3bfcf7dfab978a
Khekeh <u.keza@alustudent.com>
2022-07-04 12:33:27 -0700

    update

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -6,3 +6,10 @@ middle_word = word[1:8]
 print(f'First 3 letters: {word_first_3}')
 print(f'Last 2 letters: {word_last_2}')
 print(f'Middle word: {middle_word}')
+word = 'School'
+word_first_3 = word[:3]
+word_last_2 = word[4:]
+middle_word = word[1:5]
+print(f'First 3 letters: {word_first_3}')
+print(f'Last 2 letters: {word_last_2}')
+print(f'Middle word: {middle_word}')
