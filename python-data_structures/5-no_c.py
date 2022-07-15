80f10d9be1d40578a24c7e3d88eefffa1e19988b
Khekeh <u.keza@alustudent.com>
2022-07-15 10:51:34 -0700

    update

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = list(my_string)
-    i=0
-    for i in range(len(new_str)):
-        if new_str[i] == 'c':
-            new_str.remove('c')
-        elif new_str[i] == 'C':
-            new_str.remove('C')
-        else:
-            break
-    s = ""
-    return (s.join(new_str))
+    new = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new += i
+    return new
