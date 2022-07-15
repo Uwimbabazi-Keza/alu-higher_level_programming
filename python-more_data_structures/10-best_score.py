8b160f309b7f6a99fdb1fcae7a15625706f73c5f
Khekeh <u.keza@alustudent.com>
2022-07-15 13:26:14 -0700

    update

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return (max(a_dictionary))
+        maximum = (max(a_dictionary, key=a_dictionary.get))
+        if maximum == 0:
+            return None
+        else:
+            return maximum
     else:
         return None
