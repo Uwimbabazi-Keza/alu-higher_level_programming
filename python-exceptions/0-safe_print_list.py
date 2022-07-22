ee04eabcb5eec617fd7c2c646fc261d34ea1d735
KheKeh <u.keza@alustudent.com>
2022-07-22 11:26:58 -0700

    update

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,6 +4,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end= '')
     except (IndexError): 
+        print(i)
         return (i)
     else:
+        print(i)
         return (i)
