0c01c2700279627c4cfcb8a5e2640b36ac1ca808
KheKeh <u.keza@alustudent.com>
2022-07-22 14:00:10 -0700

    u

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""class type"""
+
+
 class Square:
+    """private instance attrivute size"""
     def __init__(self, size=0):
+        """size"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
