bda7cd32afb0502bdfd804407c315d3f04958103
KheKeh <u.keza@alustudent.com>
2022-07-22 14:26:57 -0700

    u

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 class Square:
+    """Square"""
     def __init__(self, size=0):
+        """size"""
         self.size = size
 
-    @property
+
     def size(self):
+        """size"""
         return (self.__size)
 
-    @size.setter
     def size(self, value):
+        """attribute size"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -17,6 +20,7 @@ class Square:
             self.__size = value
 
     def area(self):
+        """area"""
         return (self.__size * self.__size)
 
     def my_print(self):
