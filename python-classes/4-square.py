5741b3fbed13a95802c11b7f82f87c60c06a4f57
KheKeh <u.keza@alustudent.com>
2022-07-22 14:19:53 -0700

    u

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+""""private attribute"""
+
+
 class Square:
+    """Private instance size for class square"""
     __size = None
 
     def __init__(self, size=0):
+        """initilize size"""
         self.__size = size
         if type(size) != int:
             raise Exception("size must be an integer")
@@ -10,14 +15,17 @@ class Square:
             raise Exception("size must be >= 0")
 
     def area(self):
+        """area"""
         return (self.__size * self.__size)
 
     def set_size(self, value):
+        """size"""
         if type(value) != int:
             raise Exception("size must be an integer")
         self.__size = value
 
     def get_size(self):
+        """get size"""
         return self.__size
 
     size = property(get_size, set_size)
