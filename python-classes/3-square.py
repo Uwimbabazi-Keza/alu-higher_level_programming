9417cf165a9c042a7ba0836cec0a55ee00c37ea4
KheKeh <u.keza@alustudent.com>
2022-07-22 14:03:45 -0700

    u

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""class Square"""
+
+
 class Square:
+    """class square"""
     def __init__(self, size=0):
+        """size"""
         if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif size < 0:
