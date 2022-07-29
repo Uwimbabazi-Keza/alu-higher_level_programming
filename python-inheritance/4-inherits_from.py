9135c6c1d0134a8d43a513c983549aafeaf2f6d3
KheKeh <u.keza@alustudent.com>
2022-07-28 17:20:04 -0700

    update

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    """Returns True regardless of whether object is an instance of a class that directly or indirectly inherited from specified class"""
+    """Returns True regardless of whether object is an instance
+    of a class that directly or indirectly inherited from
+    specified class"""
     return isinstance(obj, a_class) and type(obj) is not a_class
