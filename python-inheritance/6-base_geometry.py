933524ea5336f19664b726651a1f71a8856f1bc2
KheKeh <u.keza@alustudent.com>
2022-07-28 17:28:34 -0700

    update

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module: class BaseGeometry with area function"""
+
+
+class BaseGeometry:
+    """class BaseGeometry with function area"""
+    def area(self):
+        raise Exception("area() is not implemented")
