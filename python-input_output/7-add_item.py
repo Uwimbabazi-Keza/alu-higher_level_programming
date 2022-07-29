620edd971b132436532a6ed10059f9701e2e6602
KheKeh <u.keza@alustudent.com>
2022-07-29 14:08:29 -0700

    update

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """Function that writes object to txt file using JSON representation"""
 import json
+import sys
 
 
-def save_to_json_file(my_obj, filename):
-    """writes an Object to txt file"""
-    with open(filename, 'w', encoding='utf-8') as f:
-        return json.dumps(my_obj)
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+my_list = []
+if path.isfile("add_item.json"):
+    my_list = load_from_json_file("add_item.json")
+save_to_json_file(my_list + sys.argv[1:], "add_item.json")
