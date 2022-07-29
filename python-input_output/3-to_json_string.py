1037916c34ec0fa2a4a99c5f5f09623181a6bcaa
KheKeh <u.keza@alustudent.com>
2022-07-29 12:39:28 -0700

    update

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function that returns JSON representation of an object"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """view JSON str representation"""
+    return json.dumps(my_obj)
+
