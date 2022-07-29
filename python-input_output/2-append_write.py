3b3f70de1989dc4626839651ca2beb8a3f74a5e2
KheKeh <u.keza@alustudent.com>
2022-07-29 12:11:44 -0700

    update

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that appends a string at end of text file
+and returns char count added"""
+
+
+def append_write(filename="", text=""):
+    """Appends string to end of txt file"""
+    with open(filename,'a', encoding='utf-8') as f:
+        return f.write(text)
