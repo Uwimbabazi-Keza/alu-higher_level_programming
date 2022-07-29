37379ded167c3e1bc7342641606a04800637b797
KheKeh <u.keza@alustudent.com>
2022-07-29 11:29:17 -0700

    update

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """using with statemnt for exception handling"""
-    with open(filename, 'r', encoding="utf-8") as f:
-        return f.read()
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
