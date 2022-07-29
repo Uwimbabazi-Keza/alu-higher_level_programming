412ac943e2acacb3a83df6a3f09652948483a524
KheKeh <u.keza@alustudent.com>
2022-07-28 18:35:25 -0700

    update

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-    
+
     def area(self):
         """pass number through area method"""
         return self.__size ** 2
