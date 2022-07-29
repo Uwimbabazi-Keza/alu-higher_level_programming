a3bae0f673343f66cb355bb08c3755c60f00ce85
KheKeh <u.keza@alustudent.com>
2022-07-29 09:08:33 -0700

    update

--- python-network_0/6-peak.py
+++ python-network_0/6-peak.py
@@ -13,7 +13,8 @@ def find_peak(list_of_integers):
     if l <= 2:
         return max(list_of_integers)
     elif list_of_integers[mid - 1] <= list_of_integers[mid]\
-            and list_of_integers[mid] >= list_of_integers[mid + 1]:
+            and list_of_integers[mid] >= \
+            list_of_integers[mid + 1]:
         return list_of_integers[mid]
 
     elif list_of_integers[mid] < list_of_integers[mid - 1]:
