3384f872c3c5d849756d3239b084363dc018fecc
KheKeh <u.keza@alustudent.com>
2022-07-28 18:03:48 -0700

    update

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module: Task 10- square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class square inherit from task 9"""
+
+    def __init__(self, size):
+        """initialize"""
+        self.integer_validator("size", size)
+        self.__size = size
+    def area(self):
+        """pass number through area method"""
+        return self.__size * self__size
