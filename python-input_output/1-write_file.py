c3b9e3835d258f9bfeca3a4e02b7abf1445295e8
KheKeh <u.keza@alustudent.com>
2022-07-29 11:54:48 -0700

    update

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,7 +2,8 @@
 """Function that writes a str to a text file
 and returns char count"""
 
+
 def write_file(filename="", text=""):
     """"writes string to text file"""
-    with open(filename,  encoding='utf8') as f:
+    with open(filename, 'w', encoding='utf8') as f:
         return f.write(text)
