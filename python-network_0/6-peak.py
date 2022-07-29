cbd33141d3bbfeafb38c9660dd2ecb8a13a39a09
KheKeh <u.keza@alustudent.com>
2022-07-29 09:26:26 -0700

    update

--- python-network_0/6-peak.py
+++ python-network_0/6-peak.py
@@ -3,21 +3,22 @@
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers"""
-    l = len(list_of_integers)
-    if l % 2 == 0:
-        mid = (l // 2) - 1
-    else:
-        mid = (l // 2)
+    """Finds a peak in a disorgainized list"""
+     if len(list_of_integers) == 0:
+        return None
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
 
-    if l <= 2:
-        return max(list_of_integers)
-    elif list_of_integers[mid - 1] <= list_of_integers[mid]\
-            and list_of_integers[mid] >= \
-            list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-
-    elif list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    x = len(list_of_integers) - 1
+    l = 0
+    int_list = list_of_integers
+    while x > l:
+        h = (x + l) // 2
+        if my_list[h] <= my_list[h + 1]:
+            l = h + 1
+        elif my_list[h] <= my_list[h - 1]:
+            x = h - 1
+        elif my_list[h] >= my_list[h + 1] and my_list[h]\
+                >= my_list[h - 1]:
+            return my_list[h]
+    return my_list[l]
