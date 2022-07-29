1b3f7b5de0a3cb3664223f96ad847b6ed35e1d06
KheKeh <u.keza@alustudent.com>
2022-07-29 11:24:41 -0700

    update

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Function that reads a text file"""
+
+
 def read_file(filename=""):
+    """using with statemnt for exception handling"""
     with open('filename', encoding="utf-8") as f:
         return f.read()
