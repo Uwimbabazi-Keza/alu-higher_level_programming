04b909bfdfadb3990b008eaa192de0996ddc3c2c
KheKeh <u.keza@alustudent.com>
2022-07-29 13:15:20 -0700

    update

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,10 +6,9 @@ import json
 """imports json module"""
 
 
-def save_to_json_file(my_obj, filename):
-    
-    """converts object from json file to pyobject"""
-
+def save_to_json_file(my_obj, filename): 
+    """converts object from 
+    json file to pyobject"""
     with open(filename, 'w', encoding='utf-8') as f:
         o = json.loads(my_obj)
         return f.write(o)
