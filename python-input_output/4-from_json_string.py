e871808af964e913d70dbc7ccc2f4938e7f07aa4
KheKeh <u.keza@alustudent.com>
2022-07-29 12:46:22 -0700

    update

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Function that returns an object represented by a JSON str"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """returns object"""
+    return json.load(my_str)
+    
+
