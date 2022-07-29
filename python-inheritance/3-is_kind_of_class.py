5f07c329e7e0e723613328e9d7a3f036a7899fa4
KheKeh <u.keza@alustudent.com>
2022-07-28 17:01:36 -0700

    update

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""initializate module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks if the object is an instance or if the object is an instance of a class that inherited from, the specified class"""
+    return isinstance(obj, a_class)
