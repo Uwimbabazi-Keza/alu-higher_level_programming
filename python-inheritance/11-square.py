4f851467fe91f5ae5199fb3bfe37549fe1a48591
KheKeh <u.keza@alustudent.com>
2022-07-28 18:09:36 -0700

    update

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module: Task 11, class Square inherits from Rectangle class Task 9"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class square"""
+
+    def __init__(self, size):
+        """initialize"""
+        self.integer_validator("size", size)
+        self.__size = size
+        
+    def area(self):
+        """return area of square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """returns str"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
