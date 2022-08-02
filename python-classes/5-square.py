d7d1654ef8f3a071f6d7f841231cfc48d0e4856e
KheKeh <u.keza@alustudent.com>
2022-08-02 15:08:59 -0700

    update

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -20,7 +20,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def my_print(self):
         """prints area"""
