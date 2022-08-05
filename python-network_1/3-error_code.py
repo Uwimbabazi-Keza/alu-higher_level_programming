f2e1532de37dc5d57d281169bc2831a39f6f33bf
KheKeh <u.keza@alustudent.com>
2022-08-05 06:17:32 -0700

    update

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -10,11 +10,13 @@ if __name__ == "__main__":
     """"task 3"""
     url = sys.argv[1]
     req = urllib.request.Request(url)
+
+    """raises exception"""
     try:
         with urllib.request.urlopen(req) as r:
             r = response.read()
             print(r.decode("ascii"))
     except urllib.error.HTTPError as error:
         print("Error code: {}".format(error.code))
-    except: urllib.error.URLError as error:
+    except urllib.error.URLError as error:
         print(error.reason)
