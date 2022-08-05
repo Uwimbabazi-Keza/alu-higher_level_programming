1a3bc9452bec13fdf9e32a452243fcb81ec0e73a
KheKeh <u.keza@alustudent.com>
2022-08-05 12:27:00 -0700

    update

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- creates a new table: second_table
+CREATE TABLE IF NOT EXISTS second_table(
+id INT DEFAULT NULL,
+name VARCHAR(256),
+score INT
+);
+-- inserts four new records into second_table
+INSERT INTO `second_table`(`id`, `name`, `score`)
+VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
