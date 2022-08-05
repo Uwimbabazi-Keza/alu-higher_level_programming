a0cecfd174f33e0ee5fef0ef6b5b7b65b693455f
KheKeh <u.keza@alustudent.com>
2022-08-05 05:35:57 -0700

    update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,8 +2,8 @@
 """Python script fetches https://intranet.hbtn.io/status"""
 import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    x = response.read()
+    content = response.read()
 
-print('Body response:\n\t- type: {}'.format(type(x)))
-print('\t- x: {}'.format(x))
-print('\t- utf8 x: {}'.format(x.decode('utf-8')))
+print('Body response:\n\t- type: {}'.format(type(content)))
+print('\t- content: {}'.format(content))
+print('\t- utf8 content: {}'.format(content.decode('utf-8')))
