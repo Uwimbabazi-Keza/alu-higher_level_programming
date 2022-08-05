16c36db67bffaf702781689dec3cdd546aef22e5
KheKeh <u.keza@alustudent.com>
2022-08-05 10:29:11 -0700

    update

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -4,12 +4,21 @@ and uses the GitHub API to display your id"""
 
 import requests
 import sys
+import requests
+import requests.auth
+import sys
 
-url = 'https://api.github.com/user'
-
-if __name__ == "__main__":
-    res = requests.get(url, auth=(sys.argv[1], sys.argv[2])).json()
-    if 'id' in res:
-        print(res['id'])
-    else:
-        print(None)
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get(
+        url="https://api.github.com/user",
+        auth=(requests.auth.HTTPBasicAuth(
+            username,
+            password
+        )))
+    try:
+        json_response = response.json()
+        print("{}".format(json_response["id"]))
+    except:
+        print(None))
