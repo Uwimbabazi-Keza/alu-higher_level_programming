5e46375daaaf95f62f736dce36a11bf8d8805df7
KheKeh <u.keza@alustudent.com>
2022-08-05 06:39:02 -0700

    update

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the
-URL & displays the body of the response"""
+"""takes in a URL, sends a request to the URL
+& displays the body of the response"""
 
 import requests
 import sys
 
-if __name__ == '__main__':v
+if __name__ == '__main__':
     r = requests.get(argv[1])
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
