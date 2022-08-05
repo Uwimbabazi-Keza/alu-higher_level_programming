fddab68593ab8ef0bb78bc5ac6e35e2cc24656c4
KheKeh <u.keza@alustudent.com>
2022-08-05 06:45:24 -0700

    update

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -4,18 +4,19 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 1:
-        a_dict = {'q': ''}
-    elif len(sys.argv) > 1:
-        a_dict = {'q': sys.argv[1]}
-
+    url = "http://0.0.0.0:5000/search_user"
     try:
-        req = requests.post('http://0.0.0.0:5000/search_user', a_dict)
-        if req.json():
-            print('[{}] {}'.format(req.json().get('id'),
-                                   req.json().get('name')))
-        else:
-            print('No result')
-
-    except ValueError:
+        ar = sys.argv[1]
+    except Exception:
+        ar = ""
+    q = {"q": ar}
+    r = requests.post(url, data=q)
+    try:
+        result = r.json()
+    except Exception:
         print("Not a valid JSON")
+        exit()
+    try:
+        print("[{}] {}".format(result['id'], result['name']))
+    except Exception:
+        print("No result")
