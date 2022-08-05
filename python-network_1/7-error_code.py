63134337185d14a3f217b6bde730ae08bd312921
KheKeh <u.keza@alustudent.com>
2022-08-05 06:36:50 -0700

    update

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""takes in a URL, sends a request to the
+URL & displays the body of the response"""
+
+import requests
+import sys
+
+if __name__ == '__main__':v
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
