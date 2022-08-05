8573f30099278492aeab838c6a88bba933b43155
KheKeh <u.keza@alustudent.com>
2022-08-05 07:45:55 -0700

    update

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -6,7 +6,17 @@ import requests
 import sys
 import requests.auth
 
-if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get(
+        url="https://api.github.com/user",
+        auth=(requests.auth.HTTPBasicAuth(
+            username,
+            password
+        )))
+    try:
+        json_response = response.json()
+        print("{}".format(json_response["id"]))
+    except:
+        print(None))
