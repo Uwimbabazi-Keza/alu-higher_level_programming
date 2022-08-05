f7350c5376f2b0653b0491a9476eca54e052ea45
KheKeh <u.keza@alustudent.com>
2022-08-05 14:34:56 -0700

    update

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
--- Lists all the cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states ON cities.state_id = states.id
+-- lists  all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+RIGHT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASCd
