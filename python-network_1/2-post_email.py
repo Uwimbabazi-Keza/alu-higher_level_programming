d59dacb648ed3bc615f3b3b8dc2d2fdd1fbd64f1
KheKeh <u.keza@alustudent.com>
2022-08-05 05:55:48 -0700

    update

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-""" takes in a URL and an email,
-sends a POST request to the passed
-URL with the email as a parameter,
-and displays the body of the response
-(decoded in utf-8)"""
+"""takes in a URL and an email, sends a
+POST request to the passed URL with the
+email as a parameter, and displays the body of the response"""
 
 import urllib.request
 import sys
@@ -16,4 +14,5 @@ if __name__ == '__main__':
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         r = response.read()
-      print("{}".format(r.decode("utf-8")))
+        """"decodes response"""
+        print("{}".format(r.decode("utf-8")))
