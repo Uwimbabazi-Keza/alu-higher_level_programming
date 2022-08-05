827186a11cc940949909b91d16f3594405679642
KheKeh <u.keza@alustudent.com>
2022-08-05 14:43:53 -0700

    update

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -5,9 +5,11 @@
 import requests
 import sys
 
-if __name__ == '__main__':
-    r = requests.get(argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+if __name__ == "__main__":
+
+    req = requests.get(sys.argv[1])
+
+    if req.status_code >= 400:
+        print("Error code:", req.status_code)
     else:
-        print(r.text)
+        print(req.text)
