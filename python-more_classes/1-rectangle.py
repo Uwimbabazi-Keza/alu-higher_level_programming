d8241afee062f4ef94da98448ce814d189c6e393
KheKeh <u.keza@alustudent.com>
2022-08-08 08:07:41 -0700

    update

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Real definition of a rectangle"""
+
+
+class Rectangle:
+    """class Rectangle: intializing ht and width"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    """width getter"""
+    def width(self):
+        return self.__width
+
+    @width.setter
+    """sets value width"""
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError ("width must be an integer")
+        if value < 0:
+            raise ValueError ("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    """height getter"""
+    def height(self):
+        return self.__height
+
+    @height.setter
+    """sets height"""
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError ("height must be an integer")
+        if value < 0:
+            raise ValueError ("height must be >= 0")
+        else:
+            self.__height = value
