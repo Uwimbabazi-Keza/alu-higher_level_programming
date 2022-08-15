c8002dd5d9687f496ed1863621d1c8775879e15e
KheKeh <u.keza@alustudent.com>
2022-08-14 17:56:38 -0700

    update

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-"""class square that prints area in #"""
+"""
+Printing a
+Square
+"""
 
 
 class Square:
-    """class that prints area of square in #"""
+    """ Intialize """
     def __init__(self, size=0):
-        """Initialize"""
-        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
-        """Gets size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """size equals value"""
-        if (type(value)) is not int:
+        if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
+    def area(self):
+        return self.__size * self.__size
+
     def my_print(self):
-        """prints area"""
-        def area(self):
-        """gets area"""
-            if self == 0:
-                print()
-            """accounts for 0"""
-            else:
-                for i in range(self):
-                    a = '#'
-                    print(a*self)
-        return area(self.__size)
+        if self.__size == 0:
+            print('')
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print('#', end='')
+            print('')
