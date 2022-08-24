caf07d3d25d946e9a9d5d244945146d3aded76cc
KheKeh <u.keza@alustudent.com>
2022-08-24 08:56:23 -0700

    update

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module contains function that prints our a square
+"""
+
+
+def print_square(size):
+    """
+    prints square
+    """
+    if type(size) is not int:
+            raise TypeError("size must be an integer")
+    if size < 0:
+            raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+            raise TypeError("size must be an integer")
+
+    if size == 0:
+        print('')
+    for i in range(size):
+        for i in range(size):
+            print('#', end='')
+        print('')
