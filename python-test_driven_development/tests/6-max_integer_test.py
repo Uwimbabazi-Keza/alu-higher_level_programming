dd7189c855dba694148f1dc485da1a96684b8b11
KheKeh <u.keza@alustudent.com>
2022-08-24 17:10:34 -0700

    update

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,27 +9,15 @@ import unittest
 import unittest module
 """
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+max_integer = __import__('6-max_integer').max_integer
 
 class max_int_tests(unittest.TestCase):
+    """
+    tests
+    """
     def max_integer(self):
         self.assertEqual(max_integer([5, 2, 10, 4]), 10)
         self.assertEqual(max_integer([9, 2, -10, 4]), 9)
         self.assertRaises(max_integer([5, 2, '10', 4]), (TypeError))
         self.assertRaises(max_integer([5, 2.3, 10.8, 4]), (TypeError))
-        self.assertRaises(max_integer([]), None)
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertRaises(max_integer([]), None))
