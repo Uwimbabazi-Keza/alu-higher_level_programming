19edaa823f65dbeb3d3857895a5bede29496e6bb
KheKeh <u.keza@alustudent.com>
2022-08-24 18:22:19 -0700

    update

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -13,14 +13,24 @@ class TestMaxInteger(unittest.TestCase):
     tests
     """
     def positive_int_test(self):
+        """Positive int test"""
         self.assertEqual(max_integer([5, 2, 10, 4]), 10)
 
     def negative_int_test(self):
-        self.assertEqual(max_integer([9, 2, -10, 4]), 9)
+        """Negative int test"""
+        self.assertEqual(max_integer([-9, -2, -10, -4]), -2)
 
-    def empty_list(self)
+    def empty_list(self):
+        """Empty list"""
         self.assertRaises(max_integer([]), None)
 
-if __name__== "__main__":
-    if __name__ == '__main__':
+    def middle_test(self):
+        """"middle test"""
+        self.assertEqual(max_integer([2, 3, 8, 4, 5]), 8)
+
+    def one_case(self):
+        """"One case"""
+        self.assertEqual(max_integer([1]), 1)
+
+if __name__ == '__main__':
     unittest.main()
