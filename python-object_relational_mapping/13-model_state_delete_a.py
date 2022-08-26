a3df21f29c00375a27d47bb60f0eaa6598041844
KheKeh <u.keza@alustudent.com>
2022-08-26 13:46:28 -0700

    update

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -6,7 +6,7 @@ name containing the letter a from the database hbtn_0e_6_usa
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
