6ae95e338d6280d8461124fcab403af1266aaaf1
KheKeh <u.keza@alustudent.com>
2022-08-26 11:05:38 -0700

    update

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" SQL injection to delete all records of a table"""
+
+
+import sys
+import MySQLdb
+
+
+def sql_injection():
+    """avoiding MySQL injections"""
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name=%s; ORDER BY id", (sys.argv[4],))
+    collection = c.fetchall()
+    for states in collection:
+        print(states)
+
+
+if __name__ == "__main__":
+    sql_injection()
