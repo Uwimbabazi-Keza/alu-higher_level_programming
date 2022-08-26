ff2886a8fbd0b3c77d118b077e2f7ab91f174c66
KheKeh <u.keza@alustudent.com>
2022-08-26 13:47:46 -0700

    update

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that deletes all State objects with 
+"""script that deletes all State objects with
 name containing the letter a from the database hbtn_0e_6_usa
 """
 
