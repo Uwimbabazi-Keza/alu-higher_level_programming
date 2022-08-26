e1329f5e7b5a63a0404c4ede90947301c49c1521
KheKeh <u.keza@alustudent.com>
2022-08-25 23:55:54 -0700

    update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -35,6 +35,7 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
+        """updates attributes"""
         if args is not None and len(args) != 0:
             if len(args) >= 1:
                 self.id = args[0]
