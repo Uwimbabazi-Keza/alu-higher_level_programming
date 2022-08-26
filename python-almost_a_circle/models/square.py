398d460cc4cc8581c8e5ff53be02734bd5d3ee6d
KheKeh <u.keza@alustudent.com>
2022-08-25 23:35:33 -0700

    update

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module contains class square that inherits from Rectangle
+"""
+
+
+from models.square import Square
+
+class Square(Rectangle):
+    """sub class of Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """return [Square] (<id>) <x>/<y> - <size>"""
+        return ('[Square] ({d}) {d}/{d} - {d}\
+                .format(self.id, self.x, self.y, self.width))
+
