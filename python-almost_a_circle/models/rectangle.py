c6f98c51bcd24c3fb1f2733e13c9aea86a85e36d
KheKeh <u.keza@alustudent.com>
2022-08-25 18:11:47 -0700

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,54 +21,54 @@ class Rectangle(Base):
     def width(self):
         """gets width attribute"""
         return self.__width
-    
+
     @width.setter
     def width(self, width):
         """sets width attribute"""
         if type(width) is not int:
             raise TypeError("width must be an integer")
-        elif width <= 0:
+        if width <= 0:
             raise ValueError("width must be > 0")
-        else:
-            self.__width = width
-            
+        self.__width = width
+
     @property
     def height(self):
         """return height attribute"""
         return self.__height
-    
+
     @height.setter
     def height(self, height):
         """sets height attribute"""
         if type(height) is not int:
             raise TypeError("height must be an integer")
-        elif height <= 0:
+        if height <= 0:
             raise ValueError("height must be > 0")
-        else:
-            self.__height = height
-            
+        self.__height = height
+
     @property
     def x(self):
         """gets x attribute"""
         return self.__x
-    
+
     @x.setter
     def x(self, x):
         """sets x attribute"""
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
         if height < 0:
             raise ValueError("x must be >= 0")
-        else:
-            self.__x = x
-            
+        self.__x = x
+
     @property
     def y(self):
         """gets y attribute"""
         return self.__y
-    
+
     @y.setter
     def y(self, y):
         """sets y attribute"""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
         if y < 0:
             raise ValueError("y must be >= 0")
-        else:
-            self.__y = y
+        self.__y = y
