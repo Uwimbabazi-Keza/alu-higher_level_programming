b806ac232e189dee8449e120a0a2ed3ffa39d883
KheKeh <u.keza@alustudent.com>
2022-08-26 08:53:24 -0700

    update

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all states with a
+"""
+script that lists all states with a
 name starting with N (upper N) from the
 database hbtn_0e_0_usa
 """
@@ -10,7 +11,9 @@ import MySQLdb
 
 
 def states_starting_with_N:
-    """lists states starting with N"""
+    """
+    lists states starting with N
+    """
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     cursor.execute("SELECT * FROM states WHERE state REGEXP 'N'")
@@ -20,4 +23,4 @@ def states_starting_with_N:
 
 
 if __name__ == "__main__":
-    states()
+    states_starting_with_N()
