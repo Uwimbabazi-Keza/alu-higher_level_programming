9e0f6cbe9e4d4a2aa85e2161c2bec37ecf881528
KheKeh <u.keza@alustudent.com>
2022-08-26 09:05:57 -0700

    update

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ def states_starting_with_N():
     """
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE state REGEXP 'N'")
+    c.execute("SELECT * FROM states WHERE state REGEXP 'N'" "ORDER BY id")
     collect_N_states = c.fetchall()
     for state in collect_N_states:
         print(state)
